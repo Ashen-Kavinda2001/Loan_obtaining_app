5859ff876498af6d447e107314df33a7e1cb0039
Achintha S. Gunawardhana <achintha.s.gunawardhana@gmail.com>
2025-12-23 02:08:56 +0530

    Title name chamged
    I changed the app title

--- loan_obtaining_app/lib/main.dart
+++ loan_obtaining_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'A_shen New app',
+      title: 'Loan_Obtaining_App',
       theme: ThemeData(
         // This is the theme of your application.
         //
