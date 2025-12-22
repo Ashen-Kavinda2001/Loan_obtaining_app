841fe1093c4b8f08910ef3c36ceb8993cd69d57b
Achintha S. Gunawardhana <achintha.s.gunawardhana@gmail.com>
2025-12-23 01:47:43 +0530

    Update main.dart
    Change name

--- loan_obtaining_app/lib/main.dart
+++ loan_obtaining_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Loan_obtaining_app',
+      title: 'Achintha New app',
       theme: ThemeData(
         // This is the theme of your application.
         //
