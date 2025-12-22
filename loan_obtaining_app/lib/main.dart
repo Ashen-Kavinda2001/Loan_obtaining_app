44a421f1e0f08fb6a524f68bde5db3d43a20cdfd
Ashen-Kavinda2001 <kavi2shen@gmail.com>
2025-12-23 01:59:24 +0530

    new update
    newww

--- loan_obtaining_app/lib/main.dart
+++ loan_obtaining_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ashen New app',
+      title: 'A_shen New app',
       theme: ThemeData(
         // This is the theme of your application.
         //
